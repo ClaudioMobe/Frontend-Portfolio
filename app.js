bfed5019533d96c24b2b6061a5f5618c1a1f0ec0
Claudio Mobe <claudiomobe@gmail.com>
2022-03-29 22:15:22 -0600

    First commit of Stitle

---
+++ app.js
@@ -0,0 +1,8 @@
+const loginBtn=document.getElementById("login");
+
+const desplegar=()=>{
+    let newdiv= loginBtn.createElement("DIV");
+    newdiv.innerHTML("Holaldjlajkfla");
+};
+
+loginBtn.addEventListener("click",desplegar);
