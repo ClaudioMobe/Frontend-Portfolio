a54ea4f69976212ae5d8bb55342c27e16d349ef2
Claudio Mobe <claudiomobe@gmail.com>
2022-04-01 12:57:44 -0600

    This version includes some JS in the page

--- app.js
+++ app.js
@@ -1,8 +1,22 @@
 const loginBtn=document.getElementById("login");
+const signBtn=document.getElementById("signup");
+const box= document.querySelector(".enter-data");
+const main=document.getElementById("main");
+const footer=document.getElementById("footer");
 
 const desplegar=()=>{
-    let newdiv= loginBtn.createElement("DIV");
-    newdiv.innerHTML("Holaldjlajkfla");
+    if (box.style.position=="sticky"){
+        box.style.position= "absolute";
+        box.style.top= "-100%";
+        main.style.top= "0";
+        footer.style.top= "0";
+    }else{
+    box.style.position="sticky";
+    box.style.top= "70px";
+    main.style.top= "-165px";
+    footer.style.top= "-165px";
+    }
 };
 
 loginBtn.addEventListener("click",desplegar);
+signBtn.addEventListener("click",desplegar);
